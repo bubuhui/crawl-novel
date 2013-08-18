02fca696d242a2c650439fe6a3dfacef6cfa85c9
bubuhui <takezhang@126.com>
2013-08-18 21:48:58 +0800

    add novel beans

---
+++ crawl-novel-common/src/main/java/com/zs/novel/bean/SearchSite.java
@@ -0,0 +1,38 @@
+package com.zs.novel.bean;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-8-18
+ * Time: 下午9:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class SearchSite {
+    private long id;
+    private String name;
+    private String url;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ crawl-novel-common/src/main/java/com/zs/novel/bean/SourceSite.java
@@ -0,0 +1,40 @@
+package com.zs.novel.bean;
+
+/**
+ * 小说来源网站
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-8-18
+ * Time: 下午9:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class SourceSite {
+    private long id;
+    private String name;
+    private String url;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
