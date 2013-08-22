a4ec2c0e12f09224d0093da1436e3c83f04853be
bubuhui <takezhang@126.com>
2013-08-22 17:49:38 +0800

    add

---
+++ crawl-novel-server/src/test/java/com/zs/ZsTest.java
@@ -0,0 +1,12 @@
+package com.zs;
+
+/**
+ * 
+ * @project crawl-novel-server
+ * @author zs
+ * @date 2013年8月22日
+ * Copyright (C) 2010-2013 www.2caipiao.com Inc. All rights reserved.
+ */
+public class ZsTest {
+
+}
