66954b93194aabab043c6b4152fa61c280661362
bubuhui <takezhang@126.com>
2013-08-23 17:55:28 +0800

    add service

---
+++ crawl-novel-server/src/main/java/com/zs/crawl/service/CrawlTestServiceImpl.java
@@ -0,0 +1,12 @@
+package com.zs.crawl.service;
+
+/**
+ * 
+ * @project crawl-novel-server
+ * @author zs
+ * @date 2013年8月23日
+ * Copyright (C) 2010-2013 www.2caipiao.com Inc. All rights reserved.
+ */
+public class CrawlTestServiceImpl{
+
+}
